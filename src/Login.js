64fedb0a26584e5ceb2fdee192aa8afd733bcef2
Christopher Johnston <cmjohnston@gmail.com>
2021-01-04 12:00:37 -0600

    Create React App

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+  return (
+    <div>
+      {/* Spotify logo */}
+      {/* Login with spotify button */}
+    </div>
+  )
+}
+
+export default Login
+
